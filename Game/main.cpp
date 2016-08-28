7844982f9deefaac1265da5a231fa7411e131c11
Kamil Burzyński <burzynski.kamil@gmail.com>
2016-08-28 22:30:54 +0200

    Działający przykład użycia

--- Game/main.cpp
+++ Game/main.cpp
@@ -15,7 +15,6 @@
 #include <MtFIFO/FIFOOutput.hpp>
 
 //Del
-#include <typeinfo>
 #include <iostream>
 
 using namespace mtfifo;
@@ -30,13 +29,6 @@ int main() {
 
 #include <boost/any.hpp>
 #include <boost/none.hpp>
-	boost::any a = input.get();
-	boost::any any = 12;
-	output.put(any);
-	std::cout<< output.size() << std::endl;
-	std::cout<< boost::any_cast<int>(input.get()) << std::endl;
-	std::cout<< output.size() << std::endl;
-
 	//POD - plain old data
 	struct Ex2 {
 	    int n;
@@ -51,23 +43,21 @@ int main() {
 
 	output.put(aa);
 
-	boost::any t = input.get();
-
-	try{
-		if(!t.empty()){
-			const std::type_info &ti = t.type();
-	    	std::cout << ti.name() << '\n';
+	for(int i = 0; i < 2; ++i){
+		boost::any t = input.get();
+		try{
 			Ex2 ee = boost::any_cast<Ex2>(t);
 			std::cout <<  ee.n << std::endl;
+		}catch (boost::bad_any_cast &e){
+			std::cout << "Bad casting " << std::endl;
 		}
-	}catch (boost::bad_any_cast &e){
-		std::cout << "Bad casting " << std::endl;
+		try{
+			boost::any_cast<boost::none_t>(t);
+			std::cout <<  "None" << std::endl;
+		}catch (boost::bad_any_cast &e){
+			std::cout << "Bad casting " << std::endl;
+		}
 	}
-
-
-	//Ex2 io = boost::any_cast<Ex2>(t);
-
-	//std::cout<< io.n << std::endl;
 
 	return 0;
 }
