4e6347d4d07392da61581f0d74ea9bbc0d313f59
Kamil Burzyński <burzynski.kamil@gmail.com>
2016-08-31 23:33:17 +0200

    Koniec z zabawami boostowymi

--- Game/Includes/Threads/Logging/LoggingThread.hpp
+++ Game/Includes/Threads/Logging/LoggingThread.hpp
@@ -14,20 +14,22 @@
 
 //Boost
 #include <boost/any.hpp>
-//#include <boost/log/core.hpp>
-//#include <boost/log/trivial.hpp>
-//#include <boost/log/expressions.hpp>
-//#include <boost/log/utility/setup/file.hpp>
-#include <boost/none.hpp>
-
+#include <boost/filesystem.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/path_traits.hpp>
+#include <boost/filesystem/config.hpp>
+#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/exception.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/detail/utf8_codecvt_facet.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup.hpp>
+#include <boost/none.hpp>
 
 //App
 #include <MtFIFO/FIFO.hpp>
@@ -36,12 +38,15 @@
 #include <MtFIFO/Names.hpp>
 #include <MtFIFO/Types.hpp>
 
+using namespace std;
+
 namespace thd{
 
 class LoggingThread{
 public:
 	void loggingInit(){//TODO Wyrzucic treœc do pliku .cpp
-		//boost::log::add_file_log("Pac-m.log");
+		//string ssi = "ALC";
+		//boost::log::add_file_log(boost::log::keywords::file_name = "xxy.log");
 		//boost::log::core::get()->set_filter(boost::log::trivial::severity
 		//		>= boost::log::trivial::info);
 	}
@@ -55,7 +60,7 @@ public:
 			boost::any elem = input.get();
 			try{
 				mtfifo::LogElement logElement = boost::any_cast<mtfifo::LogElement>(elem);
-				BOOST_LOG_TRIVIAL(info) << logElement.value; //TODO
+				//BOOST_LOG_TRIVIAL(static_cast<info>(boost::log::v2s_mt_nt5::trivial)) << logElement.value; //TODO
 			}catch (boost::bad_any_cast &e){
 				try{
 					boost::any_cast<boost::none_t>(elem);
